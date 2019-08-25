6094b5b4279c3b4084f8d644240b9547bc5aa725
Ahinoam <>
2019-08-25 11:47:51 +0000

    Update init.sh

--- kinesis-with-stream/init.sh
+++ kinesis-with-stream/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
-/usr/bin/kinesalite --createStreamMs=1 --deleteStreamMs=1 --updateStreamMs=1 &
+/usr/bin/kinesalite --createStreamMs=1 --deleteStreamMs=1 --updateStreamMs=1 --shardLimit 100 &
 aws --endpoint-url=http://localhost:4567 kinesis create-stream --shard-count 1 --stream-name $1
 tail -f /dev/null
